dd36ba1942a249d0a1c8aa5bf29a86764da31a1a
CodeDotJS <rushforlinux@gmail.com>
2016-01-22 01:26:25 +0530

    Exercice 2

--- 2.cpp
+++ 2.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// base class
 class Employee {
     private:
         string name;
@@ -41,10 +42,33 @@ class Employee {
             return stm.str();
         }
 };
+
+// derived class
+
+class Manager : public Employee {
+    private:
+       bool salaried;
+
+    public:
+       Manager(string name, double payRate, bool isSalaried)
+           : Employee(name, payRate)
+        {
+            salaried = isSalaried;
+        }
+
+        bool getSalaried() {
+            return salaried;
+        }
+};
+
 int main()
 {
-    Employee emp1;
-    emp1.setName("Rishi Giri");
-    cout << emp1.getName();
+    Employee emp1("Mary Smith", 15.00);
+    cout << "Employee name; " << emp1.getName() << endl;
+    cout << "Employee pay rate: " << emp1.getPay() << endl;
+    Manager emp2("Bob Brown", 1500, true);
+    cout << "Employee name; " << emp2.getName() << endl;
+    cout << "Employee pay rate: " << emp2.getPay() << endl;
+    cout << "Emp2 Salaried? " << emp2.getSalaried() << endl;
     return 0;
 }
